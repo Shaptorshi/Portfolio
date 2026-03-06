4f756655c23ce1e5cd4d09c80dc9ce648b12c46d
Shaptorshi Bhattacharya <shaptorshib@gmail.com>
2026-03-06 15:04:31 +0530

    Responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,39 @@
-import {motion} from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
+import { X, Menu } from 'lucide-react'
+import { useState } from 'react'
 
 const navbar = () => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <motion.nav initial={{y:-100}} animate={{y:0}} transition={{duration:1.2}}className='samurai-font text-2xl bg-amber-500 shadow-md sticky top-0 z-50 rounded-2xl p-2'>
+        <motion.nav initial={{ y: -100 }} animate={{ y: 0 }} transition={{ duration: 1.2 }} className='samurai-font text-2xl bg-amber-500 shadow-md sticky top-0 z-50 rounded-2xl p-2'>
             <div className='max-w-7xl mx-auto flex justify-between items-center py-4 px-6 text-gray-900 font-semibold'>
                 <h1>Portfolio</h1>
-                <ul className='flex gap-8 text-gray-900'>
+                <ul className='hidden md:flex gap-8 text-gray-900 '>
                     <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#home">Home</a></li>
                     <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#projects">Projects</a></li>
                     <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#contacts">Contacts</a></li>
                     <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#about">About</a></li>
                 </ul>
+                <button className='md:hidden' onClick={() => setOpen(!open)}>
+                    {open ? <X /> : <Menu />}
+                </button>
+
+
             </div>
+            <AnimatePresence>
+                {open &&
+                    <motion.div initial={{opacity:0,y:-20}} animate={{opacity:1,y:0}} exit={{opacity:0,y:-20}} className='absolute right-6 top-16 w-56 bg-amber-10000 backdrop-blur-xl border border-white rounded-xl p-4'>
+
+                        <ul className='samurai-font flex flex-col gap-4 mt-4 md:hidden'>
+                            <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#home">Home</a></li>
+                            <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#projects">Projects</a></li>
+                            <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#contacts">Contacts</a></li>
+                            <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#about">About</a></li>
+                        </ul>
+                    </motion.div>
+                }
+            </AnimatePresence>
         </motion.nav>
     )
 }
