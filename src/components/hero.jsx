ba5857ae1f28ee2235bab21c25137961d1f84233
Shaptorshi Bhattacharya <shaptorshib@gmail.com>
2026-03-07 01:06:38 +0530

    CTA changes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,9 +3,9 @@ import { motion } from 'framer-motion'
 
 const hero = () => {
   return (
-    <motion.section className="text-center py-32 px-6 overflow-hidden" initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
+    <motion.section className="text-center py-55 px-6 overflow-hidden" initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
       <div className='cascadia absolute text-[18vw] left-1/2 -translate-x-1/2 bg-outline select-none pointer-events-none z-0 max-w-580'>DEVELOPER</div>
-      <div className='relative z-10'>
+      <div className='z-10'>
         <p className='cascadia text-sm text-gray-500 tracking-widest uppercase'>Welcome to my portfolio</p>
         <h1 className='samurai-font mt-4 text-6xl font-bold bg-linear-to-r from-gray-900 to-gray-600 bg-clip-text'>Hi, I'm Shaptorshi👋</h1>
 
@@ -14,6 +14,14 @@ const hero = () => {
         <p className='cascadia mt-4 text-lg text-gray-500'>
           React • Node.js • Express • MongoDB
         </p>
+        <div className='mt-20 flex items-center justify-center gap-6' >
+          <motion.div whileHover={{ scale: 1.05 }} transition={{ duration: 0.5 }}>
+            <a className='bg-gray-800 text-white p-6 rounded-xl cascadia' target="_blank" href="https://drive.google.com/file/d/1SuH6PH-4u_AFhHK2sqh_royR6eWdeki5/view?usp=sharing" download={"SHAPTORSHI_BHATTACHARYA_RESUME.pdf"}>Download Resume</a>
+          </motion.div>
+          <motion.div whileHover={{scale:1.05}} transition={{duration:0.1}} className='border p-5 rounded-xl hover:bg-gray-800 hover:text-white transition-all duration-600 ease-in-out hover:scale-y-105'>
+            <a href="https://github.com/Shaptorshi" target='_blank' className='cascadia border-amber-400 p-6 rounded-xl'>Check Github</a>
+          </motion.div>
+        </div>
       </div>
     </motion.section>
   )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,9 +7,9 @@ const navbar = () => {
 
     return (
         <motion.nav initial={{ y: -100 }} animate={{ y: 0 }} transition={{ duration: 1.2 }} className='samurai-font text-2xl bg-amber-500 shadow-md sticky top-0 z-50 rounded-2xl p-2'>
-            <div className='max-w-full mx-auto flex justify-between items-center py-4 px-6 text-gray-900 font-semibold'>
+            <div className='max-w-7xl mx-auto flex justify-between items-center py-4 px-6 text-gray-900 font-semibold'>
                 <h1>Portfolio</h1>
-                <ul className='hidden md:flex gap-8 text-gray-900 '>
+                <ul className='hidden md:flex gap-6 lg:gap-8 text-gray-900 items-center text-lg'>
                     <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#home">Home</a></li>
                     <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#projects">Projects</a></li>
                     <li className='hover:text-white transition duration-300 cursor-pointer'><a href="#exp">Experience</a></li>
